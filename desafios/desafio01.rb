a4b642ce73d3e7e8476a16f3b4b3757c98b3684c
Marco Picanço <mrcpicanco@gmail.com>
2023-07-28 01:21:06 -0300

    documentation ruby and challenge one

---
+++ desafios/desafio01.rb
@@ -0,0 +1,13 @@
+=begin
+    
+Desafio 1
+Declare duas variáveis, x e y, com respectivamente 1 e 2 como valores, com apenas uma
+linha. Agora inverta os seus valores também com apenas uma linha de código. O que vamos
+fazer aqui é uma atribuição em paralelo. Resposta no final do livro!
+    
+=end
+
+x, y = 1, 2
+y, x = x, y
+
+puts x, y
